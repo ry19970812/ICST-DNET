03abe841f174b58037d09fe8aae944bc6787162a
ry19970812 <62949319+ry19970812@users.noreply.github.com>
2024-01-26 20:23:29 +0800

    Update temporal attention.py

--- main model/temporal attention.py
+++ main model/temporal attention.py
@@ -1,1 +1,109 @@
+import torch.nn.functional as F
+import pandas as pd
+from torch_geometric.utils import to_networkx
+import networkx as nx
+import matplotlib.pyplot as plt
+import torch
+import numpy as np
+import math
+import os
+import torch.utils.data as Data
+import torch.optim as optim
+import torch.nn as nn
+import argparse
+import copy
+import random
 
+
+
+
+
+class FC(nn.Module):
+    def __init__(self, input_dims, units, activations, bn_decay, use_bias=True):
+        super(FC, self).__init__()
+        if isinstance(units, int):
+            units = [units]
+            input_dims = [input_dims]
+            activations = [activations]
+        elif isinstance(units, tuple):
+            units = list(units)
+            input_dims = list(input_dims)
+            activations = list(activations)
+        assert type(units) == list
+        self.convs = nn.ModuleList([conv2d_(
+            input_dims=input_dim, output_dims=num_unit, kernel_size=[1, 1], stride=[1, 1],
+            padding='VALID', use_bias=use_bias, activation=activation,
+            bn_decay=bn_decay) for input_dim, num_unit, activation in
+            zip(input_dims, units, activations)])
+
+    def forward(self, x):
+        for conv in self.convs:
+            x = conv(x)
+        return x
+
+
+class temporalAttention(nn.Module):
+    '''
+    temporal attention mechanism
+    X:      [batch_size, num_step, num_vertex, D]
+    STE:    [batch_size, num_step, num_vertex, D]
+    K:      number of attention heads
+    d:      dimension of each attention outputs
+    return: [batch_size, num_step, num_vertex, D]
+    '''
+
+    def __init__(self, K=K, d=d, bn_decay=bn_decay, mask=True):
+        super(temporalAttention, self).__init__()
+        D = K * d
+        self.d = d
+        self.K = K
+        self.mask = mask
+        self.FC_q = FC(input_dims=1 * D, units=D, activations=F.relu,
+                       bn_decay=bn_decay)
+        self.FC_k = FC(input_dims=1 * D, units=D, activations=F.relu,
+                       bn_decay=bn_decay)
+        self.FC_v = FC(input_dims=1 * D, units=D, activations=F.relu,
+                       bn_decay=bn_decay)
+        self.FC = FC(input_dims=D, units=D, activations=F.relu,
+                     bn_decay=bn_decay)
+
+    def forward(self, X):
+        batch_size_ = X.shape[0]
+        # X = torch.cat((X, STE), dim=-1)
+        # [batch_size, num_step, num_vertex, K * d]
+        query = self.FC_q(X)
+        key = self.FC_k(X)
+        value = self.FC_v(X)
+        # [K * batch_size, num_step, num_vertex, d]
+        query = torch.cat(torch.split(query, self.K, dim=-1), dim=0)
+        key = torch.cat(torch.split(key, self.K, dim=-1), dim=0)
+        value = torch.cat(torch.split(value, self.K, dim=-1), dim=0)
+        # query: [K * batch_size, num_vertex, num_step, d]
+        # key:   [K * batch_size, num_vertex, d, num_step]
+        # value: [K * batch_size, num_vertex, num_step, d]
+        query = query.permute(0, 2, 1, 3)
+        key = key.permute(0, 2, 3, 1)
+        value = value.permute(0, 2, 1, 3)
+        # [K * batch_size, num_vertex, num_step, num_step]
+        attention = torch.matmul(query, key)
+        attention /= (self.d ** 0.5)
+        # mask attention score
+        if self.mask:
+            batch_size = X.shape[0]
+            num_step = X.shape[1]
+            num_vertex = X.shape[2]
+            mask = torch.ones(num_step, num_step)
+            mask = torch.tril(mask)
+            mask = torch.unsqueeze(torch.unsqueeze(mask, dim=0), dim=0)
+            mask = mask.repeat(self.K * batch_size, num_vertex, 1, 1)
+            mask = mask.to(torch.bool)
+            attention = torch.where(mask, attention, -2 ** 15 + 1)
+        # softmax
+        attention = F.softmax(attention, dim=-1)
+        # [batch_size, num_step, num_vertex, D]
+        X = torch.matmul(attention, value)
+        X = X.permute(0, 2, 1, 3)
+        X = torch.cat(torch.split(X, batch_size_, dim=0), dim=-1)  # orginal K, change to batch_size
+        X = self.FC(X)
+        del query, key, value, attention
+        return X
